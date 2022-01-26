2761976bcdf14cb632bc062e96cf32a3f6db484c
Chasmer <mateusz.rus97@gmail.com>
2022-01-26 19:14:59 +0100

    Przerzucenie projektu

---
+++ LordOfTheWings/LordOfTheWings.DAL/Models/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace LordOfTheWings.DAL.Models
+{
+    public class Reservation
+    {
+        [Display(Name = "Table Number")]
+        public int TableNumber { get; set; }
+        [Display(Name = "Reserved by")]
+        public string ReservedBy { get; set; }
+        [Display(Name = "Reservation date")]
+        public string ReservationDate { get; set; }
+        [Display(Name = "Reservation time")]
+        public string ReservationTime { get; set; }
+    }
+}
---
+++ LordOfTheWings/LordOfTheWings/Startup.cs
@@ -0,0 +1,73 @@
+using LordOfTheWings.DAL.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LordOfTheWings
+{
+    public class Startup
+    {
+        private const string _connectionStringName = "AppConnection";
+        public IWebHostEnvironment Environment {get;}
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
+
+            services.AddControllersWithViews();
+        }
+
+        protected virtual void ConfigureDatabase(DbContextOptionsBuilder ctxBuilder)
+        {
+            ctxBuilder.UseSqlServer(Configuration.GetConnectionString(_connectionStringName));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
